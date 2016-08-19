0d8438d7cc5671f36d6899b008f3d6c1376a96e8
vijith <vijiths2c@gmail.com>
2016-08-19 16:34:10 +0530

    variable names modified

--- exercises/mars-rover/src/mars_rover/core.clj
+++ exercises/mars-rover/src/mars_rover/core.clj
@@ -1,49 +1,47 @@
 (ns mars-rover.core)
-(defn turn-left [[a b c]]
+(defn turn-left [[x y dir]]
 	(
 		let [left_map {"N" "W", "W" "S", "S" "E", "E" "N"}]
-		[a b (get left_map c)]	
+		[x y (get left_map dir)]	
 	)
 )
 
-(defn turn-right [[a b c]]
+(defn turn-right [[x y dir]]
 	(
 		let [right_map {"N" "E", "E" "S", "S" "W", "W" "N"}]
-		[a b (get right_map c)]	
+		[x y (get right_map dir)]	
 	)
 )
 
-(defn move [[a b c]]
+(defn move [[x y dir]]
 	(cond
-  		(= c "E") [(inc a) b "E"]
-  		(= c "N") [a (inc b) "N"]
-  		(= c "W") [(dec a) b "W"]
-  		(= c "S") [a (dec b) "S"]
+  		(= dir "E") [(inc x) y "E"]
+  		(= dir "N") [x (inc y) "N"]
+  		(= dir "W") [(dec x) y "W"]
+  		(= dir "S") [x (dec y) "S"]
 	)
 )
 
-(defn take-move [[a b c d]]
+(defn take-move [[x y dir nav_literal]]
 
 	(cond
-		(= d "L") (turn-left [a b c])
-		(= d "R") (turn-right [a b c])
-		(= d "M") (move [a b c])
+		(= nav_literal "L") (turn-left [x y dir])
+		(= nav_literal "R") (turn-right [x y dir])
+		(= nav_literal "M") (move [x y dir])
 	)
 )
 
-(defn start-navigate [[a b c] d]
-	; (loop d
-	(if(= (empty? d) true)
-		[a b c]
-		(start-navigate (take-move [a b c (last d)]) (pop d))
+(defn start-navigate [[x y dir] navigation-array]
+	(if(= (empty? navigation-array) true)
+		[x y dir]
+		(start-navigate (take-move [x y dir (last navigation-array)]) (pop navigation-array))
 	)
-	 ; )
 )
 
-(defn navigate [[a b c] d]
-	(if(= (re-matches #"[LRM]*" d) nil)
+(defn navigate [[x y dir] navigation-string]
+	(if(= (re-matches #"[LRM]*" navigation-string) nil)
 		false
-		(start-navigate [a b c] (vec (reverse (clojure.string/split d #""))))	
+		(start-navigate [x y dir] (vec (reverse (clojure.string/split navigation-string #""))))	
 	)
 )
 
